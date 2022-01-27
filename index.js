b71a1bd8a4abc9c53120551e7c0b850d016c7792
jasoonmurraay <98032625+jasoonmurraay@users.noreply.github.com>
2022-01-27 16:41:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,111 @@
 const bill = document.querySelector('#bill')
+
 // const billValue = Number(bill.value);
-// const fivePct = document.querySelector('#fivePct')
-// const tenPct = document.querySelector('#tenPct')
-// const fifteenPct = document.querySelector('#fifteenPct')
-// const twentyPct = document.querySelector('#twentyPct')
-// const twentyfivePct = document.querySelector('#twentyfivePct')
+const fivePct = document.querySelector('#fivePct')
+const tenPct = document.querySelector('#tenPct')
+const fifteenPct = document.querySelector('#fifteenPct')
+const twentyPct = document.querySelector('#twentyPct')
+const twentyfivePct = document.querySelector('#twentyfivePct')
 let tip = document.querySelectorAll('input[name="tipPct"]')
 // const customPct = document.querySelector('#customPct')
 const numPeople = document.querySelector('#numPeople')
-const eachTipDisplay = document.querySelector('.eachTipAmount')
-const eachTotalDisplay = document.querySelector('.eachTotalAmount')
+// const eachTipAmount = document.querySelector('.eachTipAmount')
+// const eachTotalAmount = document.querySelector('.eachTotalAmount')
 const customInput = document.querySelector('.customInput')
 const customInputArea = document.querySelector('#customInputArea')
+const eachTipDisplay = document.querySelector('#eachTipDisplay')
+const eachTotalDisplay = document.querySelector('#eachTotalDisplay')
 
 const numPeopleErrorMsg = document.querySelector('.numPeopleErrorMsg')
 
-for (let i = 0; i < tip.length; i++) {
-    tip[i].addEventListener('change', function () {
-        let totalTip = this.value / 100;
-        console.log(totalTip)
-        if (this.value === 'custom') {
-            customInput.style.display = 'block';
-        } if (this.value !== 'custom') {
-            customInput.style.display = 'none';
-        }
-    })
+
+
+function tipPct() {
+    for (let i = 0; i < tip.length; i++) {
+        tip[i].addEventListener('change', function () {
+            let tipPct = (tip[i].value)
+        })
+    }
 }
 
+
+function calculate() {
+    const tipValue = Number(bill.value) * tipPct
+    const billPlusTip = Number(bill.value) + tipValue
+    const tipPerPerson = tipValue / Number(numPeople.value)
+    const totalPerPerson = billPlusTip / Number(numPeople.value)
+    eachTipDisplay.textContent = tipPerPerson;
+    eachTotalDisplay.textContent = totalPerPerson;
+}
+
+bill.addEventListener('change', function () {
+    calculate();
+})
+
+// for (let i = 0; i < tip.length; i++) {
+//     tip[i].addEventListener('change', function () {
+//         calculate();
+//     })
+//     tipValue();
+// }
+
 numPeople.addEventListener('change', function () {
-    if (numPeople.value <= 0) {
-        numPeopleErrorMsg.style.display = 'block'
-        const numPeopleInputArea = document.querySelector('.numPeopleInputArea')
-        numPeopleInputArea.style.border = '1px solid hsl(9, 56%, 78%)'
-        numPeopleInputArea.style.borderRadius = '4px'
-    }
+    calculate();
 })
 
-customInputArea.addEventListener('change', function () {
-    let totalTip = Number(this.value) / 100;
-    console.log(totalTip)
-})
 
-// customPct.addEventListener('change', function () {
+
+
+
+// for (let i = 0; i < tip.length; i++) {
+//     tip[i].addEventListener('change', function tipPct() {
+//         let totalTipPct = this.value / 100;
+//         console.log(totalTipPct)
+//         if (this.value === 'custom') {
+//             customInput.style.display = 'block';
+//             customInputArea.addEventListener('change', function () {
+//                 let totalTipPct = Number(this.value) / 100;
+//                 console.log(totalTipPct)
+//                 if (totalTipPct === NaN || totalTipPct < 0) {
+//                     alert('ENTER A VALID PERCENT')
+//                 }
+
+//             })
+//         } if (this.value !== 'custom') {
+//             customInput.style.display = 'none';
+//         }
+
+//     })
+// }
+
+// numPeople.addEventListener('change', function numPeople() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (numPeople.value <= 0) {
+//                 numPeopleErrorMsg.style.display = 'block'
+//                 const numPeopleInputArea = document.querySelector('.numPeopleInputArea')
+//                 numPeopleInputArea.style.border = '1px solid hsl(9, 56%, 78%)'
+//                 numPeopleInputArea.style.borderRadius = '4px'
+//             }
+//             let people = Number(this.value)
+//             console.log(people)
+//         }, delay)
+//     })
 // })
+
+// bill.addEventListener('change', function billAmount() {
+//     let billValue = Number(this.value)
+//     console.log(billValue)
+
+// })
+
+// function calculation() {
+//     let tipPct = tipPct();
+//     let people = numPeople();
+//     let billAmount = billAmount();
+//     let tipPerPerson = (billAmount + (billAmount * tipPct)) / people
+//     console.print(tipPerPerson)
+// }
+
+
+
